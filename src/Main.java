24fd4a516bce8bc785b95f28464f0316e2d99044
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-16 11:13:07 +0000

    Now catching exceptions parser and creating reports

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 import pt.up.fe.comp.jmm.JmmParser;
 import pt.up.fe.comp.jmm.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.ReportType;
+import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.specs.util.SpecsIo;
 
 import java.io.File;
@@ -9,30 +11,39 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.io.StringReader;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main implements JmmParser {
 
 
 	public JmmParserResult parse(String jmmCode) {
-		
+		List<Report> reports = new ArrayList<Report>();
+		SimpleNode root = null;
+
 		try {
 		    Jmm jmm = new Jmm(new StringReader(jmmCode));
-    		SimpleNode root = jmm.Program(); // returns reference to root node
+    		root = jmm.Program(); // returns reference to root node
             	
     		root.dump(""); // prints the tree on the screen
 			//System.out.println(root.toJson());
     	
-    		return new JmmParserResult(root, new ArrayList<Report>());
+    		return new JmmParserResult(root, reports);
 		} catch(ParseException e) {
-			throw new RuntimeException("Error while parsing", e);
+			//throw new RuntimeException("Error while parsing", e);
+			reports.add(new Report(ReportType.ERROR, Stage.SYNTATIC, e.getStackTrace()[0].getLineNumber(), e.getMessage()));
+			return new JmmParserResult(root, reports);
 		}
 	}
 
     public static void main(String[] args) throws FileNotFoundException {
 		Main temp = new Main();
 		String fileContents = SpecsIo.read(args[0]);
-		temp.parse(fileContents);
+		JmmParserResult results = temp.parse(fileContents);
+
+		for(Report r : results.getReports()){
+			System.out.println("Report: " + r.getMessage());
+		}
     }
 
 
