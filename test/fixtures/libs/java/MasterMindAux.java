f9048d15bd42733cdd8b07874740bc5156ab46ba
xavier <up201806134@fe.up.pt>
2021-05-22 12:24:00 +0100

    Improved mastermind a little

---
+++ test/fixtures/libs/java/MasterMindAux.java
@@ -0,0 +1,17 @@
+public class MasterMindAux {
+    public static void printGetResult() {
+        System.out.println("Insert the result, one number per line.");
+    }
+
+    public static void printGetGuess() {
+        System.out.println("Insert your guess, one number per line.");
+    }
+
+    public static void printWon() {
+        System.out.println("You guessed correctly.");
+    }
+
+    public static void printLost() {
+        System.out.println("You failed 10 times.");
+    }
+}
